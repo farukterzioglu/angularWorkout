ea59f2d86abdb1a1eaeb51a15dcdac97661122a9
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2018-06-09 08:37:21 +0300

    progress

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -17,7 +17,11 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes() : void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService
+    .getHeroes()
+    .subscribe( 
+      heroes => this.heroes = heroes
+    );
   }
 
   ngOnInit() {
